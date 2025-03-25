b3563693758145f4e155bc238b0d5bf402e7e877
yaseminlbc <113558226+yaseminlbc@users.noreply.github.com>
2025-03-25 14:48:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 async function getAdvice() {
   const adviceElement = document.getElementById('advice');
-  adviceElement.innerText = ''; // sıfırla
+  adviceElement.innerText = ''; 
 
   const response = await fetch('https://api.adviceslip.com/advice');
   const data = await response.json();
   const adviceText = data.slip.advice;
 
   let i = 0;
-  const speed = 40; // yazı hızı (ms)
+  const speed = 40; 
 
   function typeWriter() {
     if (i < adviceText.length) {
-      adviceElement.innerText += adviceText.charAt(i);
+      adviceElement.textContent += adviceText.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
     }
